9f55796f325f607a43191a3c4662f8ceaa39eb47
juandiego <juancho571@gmail.com>
2015-10-22 13:03:48 -0430

    la gallina

--- public/javascripts/gallinas.js
+++ public/javascripts/gallinas.js
@@ -2,8 +2,8 @@ function dibujaGallinas(){
 	
 	var top = 548;
 	var diferencia = 80;
-
-	for (var x = 0; x < 7; x++) {
+	var gallina = "images/gallina.png";
+	for (var x = 0; x < 4; x++) {
 		left = 168;		
 		z = 3;
 		if (x >=  2 && x <= 4){
@@ -13,8 +13,8 @@ function dibujaGallinas(){
 
 		for (var y = 0; y < z; y++) {			
 			
-			$("#fichas").append("<div id='ficha' class='ficha pg"+x+"-"+y+"'><img src='images/chicken.jpg'></img></div>");
-			$("#pg"+x+"-"+y).offset({
+			$("#fichas").append("<div id='ficha' class='ficha pg"+x+"-"+y+"'><img src='"+gallina+"'></img></div>");
+			$(".pg"+x+"-"+y).offset({
 				top:top,
 				left:left
 			});
